81514469720a594c1f751ceeea8c4b7574acb888
Osmon_r <symokevo@gmail.com>
2026-01-01 13:27:59 +0300

    Crete page1 document and the respective javascript

---
+++ js/app1.js
@@ -0,0 +1,3 @@
+$("document").ready(function() {
+    alert("The installation was successful!");
+});
---
+++ js/app2.js
@@ -0,0 +1,14 @@
+console.log($('div'));
+$('div').html('Hello');
+console.log($('.div2'));
+console.log($('#div3'));
+console.log($('#div3, .div2'));
+const $ele1 = $('div');
+console.log($ele1);
+$ele1.html('Hello again');
+const $ele2 = $('.div2');
+console.log($ele2);
+const $ele3 = $('#div3');
+console.log($ele3);
+const $ele4 = $('#div3, .div2');
+console.log($ele4);
