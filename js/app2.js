9f90456d1f6f2a3801396114fb6f5bc6536a9652
Osmon_r <symokevo@gmail.com>
2026-01-01 14:30:20 +0300

    Add styling

--- js/app2.js
+++ js/app2.js
@@ -12,3 +12,29 @@ const $ele3 = $('#div3');
 console.log($ele3);
 const $ele4 = $('#div3, .div2');
 console.log($ele4);
+
+$(document).ready(function() {
+    // Attach a click event elements with data-toggle attribute
+    $("[data-toggle]").click(function() {
+        const target = $(this).data("toggle");
+        alert(`Toggle ${target}`);
+    });
+
+    // Change CSS for the first item in the list
+    $("ul li:first-child").css("color", "purple");
+});
+
+
+$('#btn1').click(function() {
+    console.log($(this).parent().length);
+    console.log($(this).parents().length);
+    console.log($(this).parents());
+    console.log($(this).parents('.main'));
+    console.log($(this).parentsUntil('nav'));
+});
+
+$('#btn2').click(function() {
+    $(this).parents('.main').each(function(){
+        $(this).addClass('parentM');
+    });
+});
